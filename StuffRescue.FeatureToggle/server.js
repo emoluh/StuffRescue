1e55cc969c686a931c0b6795b1e9db0f204a5327
iminsik <iminsik@gmail.com>
2017-06-08 23:02:46 -0700

    Add tedious node modules to connect Azure database
    add tedious modules to connect Azure database
    
    remove credential

--- StuffRescue.FeatureToggle/server.js
+++ StuffRescue.FeatureToggle/server.js
@@ -1,8 +1,74 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+// Create connection to database
+var config = {
+    userName: '[username]', // update me
+    password: '[password]', // update me
+    server: '[mydbname].database.windows.net', // update me
+    options: {
+        database: '[mydatabase]', //update me
+        encrypt: true
+    }
+}
+
+const connection = new Connection(config);
+connection.on('connect', function (err) {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log('Database connneted...');
+        executeStatement();
+    }
+});
+
+function insertIntoDatabase() {
+    console.log("Inserting a brand new feature into database...");
+    request = new Request(
+        "INSERT INTO dbo.[mydatabase] (Name, Value) VALUES ('DataSource', 1)",
+        function (err, rowCount, rows) {
+            console.log(rowCount + ' row(s) inserted');
+        }
+    );
+    connection.execSql(request);
+}
+
+function queryDatabase() {
+    console.log('Reading rows from the Table...');
+
+    // Read all rows from table
+    request = new Request(
+        "SELECT ff.Name, ff.Value FROM dbo.[mydatabase] ff",
+        function (err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+        }
+    );
+
+    request.on('row', function (columns) {
+        columns.forEach(function (column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+
+    connection.execSql(request);
+}
+
+function executeStatement() {
+    request = new Request("select 42, 'hello world'", function (err, rowCount) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rowCount + ' rows');
+        }
+    });
+
+    request.on('row', function (columns) {
+        columns.forEach(function (column) {
+            console.log(column.value);
+        });
+    });
+
+    connection.execSql(request);
+}
