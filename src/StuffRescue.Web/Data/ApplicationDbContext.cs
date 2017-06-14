ff3cca1ec34e9522c4edfca3673a61703e9bfd91
Emmanuel Moluh <Emmanuel.Moluh@teksystemsgs.com>
2017-06-13 17:12:53 -0700

    Added Database Schema Diagram

--- src/StuffRescue.Web/Data/ApplicationDbContext.cs
+++ src/StuffRescue.Web/Data/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using StuffRescue.Web.Models;
 
@@ -21,6 +17,49 @@ namespace StuffRescue.Web.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable(name: "User");
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Role");
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("UserRoles");
+                //in case you chagned the TKey type
+                //  entity.HasKey(key => new { key.UserId, key.RoleId });
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("UserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("UserLogins");
+                //in case you chagned the TKey type
+                //  entity.HasKey(key => new { key.ProviderKey, key.LoginProvider });       
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("RoleClaims");
+
+            });
+
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("UserTokens");
+                //in case you chagned the TKey type
+                // entity.HasKey(key => new { key.UserId, key.LoginProvider, key.Name });
+
+            });
+
         }
     }
 }
--- src/StuffRescue.Web/Data/Migrations/20170614000815_identity.Designer.cs
+++ src/StuffRescue.Web/Data/Migrations/20170614000815_identity.Designer.cs
@@ -8,9 +8,10 @@ using StuffRescue.Web.Data;
 namespace StuffRescue.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170614000815_identity")]
+    partial class identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
