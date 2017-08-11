a31072ce55e2e02fee409ec65060ed045ee123fd
Emmanuel Moluh <Emmanuel.Moluh@teksystemsgs.com>
2017-08-11 06:19:23 -0700

    Added Dashboard And Features Skeleton Views

--- 4. User Experience Layer/StuffRescue.FeatureToggle/ClientApp/app/app.module.shared.ts
+++ 4. User Experience Layer/StuffRescue.FeatureToggle/ClientApp/app/app.module.shared.ts
@@ -4,19 +4,24 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component'
 import { HomeComponent } from './components/home/home.component';
 import { NavigatorComponent } from './components/shared/navigator/navigator.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FeaturesComponent } from './components/features/features.component'
 
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
         HomeComponent,
-        NavigatorComponent
+        NavigatorComponent,
+        DashboardComponent,
+        FeaturesComponent
     ],
     imports: [
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'features', component: FeaturesComponent },
+            { path: '**', redirectTo: 'dashboard' },
         ])
     ]
 };
---
+++ 4. User Experience Layer/StuffRescue.FeatureToggle/ClientApp/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+}
